43adff24df4b515a0a27ba529f58150cc04c8282
Antonis Christofides <antonis@antonischristofides.com>
2017-09-23 00:40:57 +0300

    Filter repairs by complete

--- frontend/src/components/ui/RepairList.jsx
+++ frontend/src/components/ui/RepairList.jsx
@@ -8,27 +8,51 @@ class RepairList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      filterByUser: 0,
       repairs: props.repairs,
     };
+    this.filters = {
+      user: 0,
+      complete: null,
+    };
     this.onChangeUserFilter = this.onChangeUserFilter.bind(this);
+    this.onChangeCompleteFilter = this.onChangeCompleteFilter.bind(this);
   }
 
   onChangeUserFilter(e) {
-    const value = parseInt(e.target.value, 10);
-    this.setState({
-      filterByUser: value,
-      repairs: this.props.repairs.filter((r) => {
-        switch (value) {
-          case -1:
-            return !r.assignedUser;
-          case 0:
-            return true;
-          default:
-            return r.assignedUser && r.assignedUser.id === value;
-        }
-      }),
+    this.filters.user = parseInt(e.target.value, 10);
+    this.applyFilters();
+  }
+
+  onChangeCompleteFilter(e) {
+    this.filters.complete = JSON.parse(e.target.value);
+    this.applyFilters();
+  }
+
+  applyFilters() {
+    let filteredRepairs = this.props.repairs.filter((r) => {
+      switch (this.filters.user) {
+        case -1:
+          return !r.assignedUser;
+        case 0:
+          return true;
+        default:
+          return r.assignedUser &&
+            r.assignedUser.id === this.filters.user;
+      }
     });
+    filteredRepairs = filteredRepairs.filter((r) => {
+      switch (this.filters.complete) {
+        case null:
+          return true;
+        case true:
+          return r.complete;
+        case false:
+          return !r.complete;
+        default:
+          throw new Error('Internal error');
+      }
+    });
+    this.setState({ repairs: filteredRepairs });
   }
 
   render() {
@@ -87,7 +111,19 @@ class RepairList extends React.Component {
                 :
                 null
               }
-              <td />
+              <td>
+                <form className="complete-filter-form">
+                  <select
+                    name="complete"
+                    defaultValue={null}
+                    onChange={this.onChangeCompleteFilter}
+                  >
+                    <option key={0} value='null' />
+                    <option key={1} value='true'>Completed</option>
+                    <option key={2} value='false'>Not completed</option>
+                  </select>
+                </form>
+              </td>
               <td />
             </tr>
             {
