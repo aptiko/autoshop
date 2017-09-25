2b9f339b9a62e70ec1de15e9f173eb2be05d36be
Antonis Christofides <antonis@antonischristofides.com>
2017-09-25 11:34:54 +0300

    Remove accidentally added django_extensions

--- autoshop_project/settings.py
+++ autoshop_project/settings.py
@@ -15,7 +15,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',
     'allauth',
